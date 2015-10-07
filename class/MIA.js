ee3696e34735631e2a86268788dad6a3d7f593cb
Sébastien VIDAL <svidal@wynd.eu>
2015-10-07 12:31:36 +0200

    réintégration du code de login dans le module de base

--- class/MIA.js
+++ class/MIA.js
@@ -2,13 +2,18 @@
 // dépendances
 	
 	var
+
 		CST_DEP_Path = require('path'),
 		CST_DEP_FileSystem = require('fs'),
 		CST_DEP_Q = require('q'),
+
 		CST_DEP_HTTPServer = require(CST_DEP_Path.join(__dirname, 'HTTPServer.js')),
 		CST_DEP_HTTPSocket = require(CST_DEP_Path.join(__dirname, 'HTTPSocket.js')),
 		CST_DEP_ChildSocket = require(CST_DEP_Path.join(__dirname, 'ChildSocket.js')),
+
+		CST_DEP_Log = require('logs'),
 		CST_DEP_Conf = require(CST_DEP_Path.join(__dirname, 'Conf.js')),
+
 		CST_DEP_SikyAPI = require('SIKY-API');
 		
 // module
@@ -18,10 +23,13 @@
 		// attributes
 			
 			var
-				m_clThis = this,
+				m_stSIKYUser,
+
 				m_clHTTPServer = new CST_DEP_HTTPServer(),
 				m_clHTTPSocket = new CST_DEP_HTTPSocket(),
 				m_clChildSocket = new CST_DEP_ChildSocket(),
+				
+				m_clLog = new CST_DEP_Log(CST_DEP_Path.join(__dirname, '..', 'logs')),
 				m_clConf = new CST_DEP_Conf(),
 				m_clSikyAPI = new CST_DEP_SikyAPI();
 				
@@ -36,32 +44,92 @@
 
 						try {
 
-							m_clHTTPServer.start(m_clConf.getConf().portweb)
-								.then(function() {
+							// events
 
-									// plugins
-										
-										var sPluginsPath = CST_DEP_Path.join(__dirname, '..', 'plugins');
+								m_clHTTPSocket
+									.onDisconnect(function(socket) {
+										socket.removeAllListeners('child.getconnected');
+										socket.removeAllListeners('child.login');
+									})
+									.onConnection(function(socket) {
 
-										CST_DEP_FileSystem.readdirSync(sPluginsPath).forEach(function (file) {
-											require(CST_DEP_Path.join(sPluginsPath, file))(m_clHTTPSocket, m_clChildSocket, m_clSikyAPI);
-										});
-
-									// start
-										
-										m_clHTTPSocket.start(m_clHTTPServer.getServer())
-											.then(function () {
-												
-												m_clChildSocket.start(m_clConf.getConf().portchildren)
-													.then(deferred.resolve)
-													.catch(deferred.reject);
-													
+										socket
+											.on('child.getconnected', function () {
+												m_clHTTPSocket.emit('child.getconnected', m_clChildSocket.getConnectedChilds());
 											})
-											.catch(deferred.reject);
+											.on('child.login', function (p_stData) {
 
-								})
-								.catch(deferred.reject);
-						
+												if (m_stSIKYUser && m_stSIKYUser.email == p_stData.email && m_stSIKYUser.password == p_stData.password) {
+													socket.emit('child.logged');
+												}
+												else {
+
+													m_clSikyAPI.login(p_stData.email, p_stData.password)
+														.then(function () {
+
+															m_stSIKYUser = {
+																token : m_clSikyAPI.getToken(),
+																email : p_stData.email,
+																password : p_stData.password
+															};
+
+															m_clLog.success('-- [socket server] logged to SIKY');
+															socket.emit('child.logged');
+															
+														})
+														.catch(function (e) {
+															
+															if(e.message) {
+																e = e.message;
+															}
+
+															m_clLog.err(e);
+															socket.emit('child.login.error', e);
+
+														});
+														
+												}
+
+											});
+
+									});
+
+								m_clChildSocket
+									.onDisconnect(function(socket) {
+										m_clHTTPSocket.emit('child.disconnected', socket.MIA);
+									})
+									.onConnection(function(socket) {
+										m_clHTTPSocket.emit('child.connection', socket.MIA);
+									});
+
+							// run
+
+								m_clHTTPServer.start(m_clConf.getConf().portweb)
+									.then(function() {
+
+										// plugins
+											
+											var sPluginsPath = CST_DEP_Path.join(__dirname, '..', 'plugins');
+
+											CST_DEP_FileSystem.readdirSync(sPluginsPath).forEach(function (file) {
+												require(CST_DEP_Path.join(sPluginsPath, file))(m_clHTTPSocket, m_clChildSocket, m_clSikyAPI);
+											});
+
+										// start
+											
+											m_clHTTPSocket.start(m_clHTTPServer.getServer())
+												.then(function () {
+													
+													m_clChildSocket.start(m_clConf.getConf().portchildren)
+														.then(deferred.resolve)
+														.catch(deferred.reject);
+														
+												})
+												.catch(deferred.reject);
+
+									})
+									.catch(deferred.reject);
+							
 						}
 						catch (e) {
 							if (e.message) {
---
+++ plugins/temperature.js
@@ -0,0 +1,24 @@
+
+// dépendances
+	
+// module
+	
+	module.exports = function (p_clHTTPSocket, p_clChildSocket) {
+
+		// constructor
+
+			p_clChildSocket
+				.onDisconnect(function(socket) {
+					socket.removeAllListeners('child.temperature');
+				})
+				.onConnection(function(socket) {
+
+					socket
+						.on('child.temperature', function (data) {
+							socket.MIA.temperature = data;
+							p_clHTTPSocket.emit('child.temperature', socket.MIA);
+						});
+
+				});
+
+	};
