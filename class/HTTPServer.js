875d64cd066abbff1b8c3467d292fc3bac904bcc
Sébastien VIDAL <svidal@wynd.eu>
2015-11-04 17:21:46 +0100

    ajout du framework express pour la partie web

--- class/HTTPServer.js
+++ class/HTTPServer.js
@@ -6,6 +6,8 @@
 		fs = require('fs'),
 		url = require('url'),
 		q = require('q'),
+		app = require('express')(),
+		http = require('http').Server(app),
 		Logs = require(path.join(__dirname, 'Logs.js'));
 		
 // module
@@ -17,7 +19,6 @@
 		// attributes
 			
 			var
-				m_clServer,
 				m_sDirWeb = path.join(__dirname, '..', 'web'),
 				m_tabWebPlugins = [],
 				m_clLog = new Logs(path.join(__dirname, '..', 'logs', 'httpserver'));
@@ -28,40 +29,40 @@
 
 				// response
 					
-					function _sendResponse(p_clResponse, p_nCode, p_sContentType, p_sMessage) {
-						p_clResponse.writeHead(p_nCode, {'Content-Type': p_sContentType});
-						p_clResponse.end(p_sMessage);
+					function _sendResponse(res, p_nCode, p_sContentType, p_sMessage) {
+						res.writeHead(p_nCode, {'Content-Type': p_sContentType});
+						res.end(p_sMessage);
 					}
 						
-						function _sendHTMLResponse(p_clResponse, p_nCode, p_sMessage) {
-							_sendResponse(p_clResponse, p_nCode, 'text/html', p_sMessage);
+						function _sendHTMLResponse(res, p_nCode, p_sMessage) {
+							_sendResponse(res, p_nCode, 'text/html', p_sMessage);
 						}
 							
-							function _404(p_clResponse) {
-								_sendHTMLResponse(p_clResponse, 404, 'Not found');
+							function _404(res) {
+								_sendHTMLResponse(res, 404, 'Not found');
 							}
 							
-							function _500(p_clResponse, p_sMessage) {
+							function _500(res, p_sMessage) {
 
 								if (p_sMessage) {
-									_sendHTMLResponse(p_clResponse, 500, p_sMessage);
+									_sendHTMLResponse(res, 500, p_sMessage);
 								}
 								else {
-									_sendHTMLResponse(p_clResponse, 500, 'Internal error');
+									_sendHTMLResponse(res, 500, 'Internal error');
 								}
 								
 							}
 						
-						function _sendJSResponse(p_clResponse, p_nCode, p_sMessage) {
-							_sendResponse(p_clResponse, p_nCode, 'application/javascript', p_sMessage);
+						function _sendJSResponse(res, p_nCode, p_sMessage) {
+							_sendResponse(res, p_nCode, 'application/javascript', p_sMessage);
 						}
 
-						function _sendCSSResponse(p_clResponse, p_nCode, p_sMessage) {
-							_sendResponse(p_clResponse, p_nCode, 'text/css', p_sMessage);
+						function _sendCSSResponse(res, p_nCode, p_sMessage) {
+							_sendResponse(res, p_nCode, 'text/css', p_sMessage);
 						}
 
-						function _sendPNGResponse(p_clResponse, p_nCode, p_sMessage) {
-							_sendResponse(p_clResponse, p_nCode, 'image/png', p_sMessage);
+						function _sendPNGResponse(res, p_nCode, p_sMessage) {
+							_sendResponse(res, p_nCode, 'image/png', p_sMessage);
 						}
 
 				// files
@@ -304,220 +305,10 @@ q
 
 					}
 					
-				// routers
-					
-					function _router(p_clResponse, p_sPathName) {
-
-						var tabURI;
-
-						try {
-
-							tabURI = p_sPathName.split('/').filter(function (sData) {
-								return ('' != sData);
-							});
-
-							switch (tabURI.length) {
-
-								case 0:
-
-									m_clLog.log('-- [HTTP server] query');
-
-									_readFile('', 'index.html')
-										.then(function (index) {
-
-											_extractPluginsTemplates()
-												.then(function(sHTML) {
-													_sendHTMLResponse(p_clResponse, 200, index.replace('{{pages}}', sHTML));
-												})
-												.catch(function (error) {
-													_500(p_clResponse, index.replace('{{pages}}', error));
-												});
-
-										})
-										.catch(function (error) {
-											_500(p_clResponse, error);
-										});
-
-								break;
-
-								default:
-
-									switch (tabURI[0]) {
-
-										case 'js':
-
-											if (tabURI[1]) {
-
-												switch (tabURI[1]) {
-
-													case 'plugins.js' :
-
-														_extractPluginsJavascripts()
-															.then(function(sJavascript) {
-																_sendJSResponse(p_clResponse, 200, sJavascript);
-															})
-															.catch(function (error) {
-																_500(p_clResponse, error);
-															});
-
-													break;
-
-													case 'children.js' :
-
-														_readAllFiles(path.join(m_sDirWeb, 'js'))
-															.then(function (data) {
-																_sendJSResponse(p_clResponse, 200, data);
-															})
-															.catch(function (error) {
-																_500(p_clResponse, error);
-															});
-
-													break;
-
-													default :
-														_404(p_clResponse);
-													break;
-
-												}
-
-											}
-											else {
-												_404(p_clResponse);
-											}
-
-										break;
-
-										case 'pictures':
-
-											switch (tabURI[1]) {
-
-												case 'favicon.png':
-
-													_readFile('pictures', 'favicon.png')
-														.then(function (data) {
-															_sendPNGResponse(p_clResponse, 200, data);
-														})
-														.catch(function (error) {
-															_500(p_clResponse, error);
-														});
-
-												break;
-
-												default:
-													_404(p_clResponse);
-												break;
-
-											}
-
-										break;
-
-										case 'libs':
-
-											switch (tabURI[1]) {
-
-												// css
-
-												case 'bootstrap.css':
-
-													_readAllFiles(path.join(m_sDirWeb, 'libs', 'bootstrap', 'css'))
-														.then(function (data) {
-															_sendCSSResponse(p_clResponse, 200, data);
-														})
-														.catch(function (error) {
-															_500(p_clResponse, error);
-														});
-
-												break;
-
-												// js
-												
-												case 'jquery.js':
-
-													_readAllFiles(path.join(m_sDirWeb, 'libs', 'jquery'))
-														.then(function (data) {
-															_sendJSResponse(p_clResponse, 200, data);
-														})
-														.catch(function (error) {
-															_500(p_clResponse, error);
-														});
-
-												break;
-												case 'bootstrap.js':
-
-													_readAllFiles(path.join(m_sDirWeb, 'libs', 'bootstrap', 'js'))
-														.then(function (data) {
-															_sendJSResponse(p_clResponse, 200, data);
-														})
-														.catch(function (error) {
-															_500(p_clResponse, error);
-														});
-
-												break;
-												case 'socketio.js':
-
-													_readFile('libs/socketio', 'socket.io.js')
-														.then(function (data) {
-															_sendJSResponse(p_clResponse, 200, data);
-														})
-														.catch(function (error) {
-															_500(p_clResponse, error);
-														});
-
-												break;
-												case 'angular.js':
-
-													_readFile('libs/angularjs', 'angular.min.js')
-														.then(function (data) {
-															_sendJSResponse(p_clResponse, 200, data);
-														})
-														.catch(function (error) {
-															_500(p_clResponse, error);
-														});
-
-												break;
-												case 'angular-modules.js':
-
-													_readAllFiles(path.join(m_sDirWeb, 'libs', 'angularjs', 'modules'))
-														.then(function (data) {
-															_sendJSResponse(p_clResponse, 200, data);
-														})
-														.catch(function (error) {
-															_500(p_clResponse, error);
-														});
-
-												break;
-
-												default:
-													_404(p_clResponse);
-												break;
-
-											}
-
-										break;
-
-										default:
-											_404(p_clResponse);
-										break;
-
-									}
-
-								break;
-
-							}
-							
-						}
-						catch (e) {
-							m_clLog.err(e);
-							_500(p_clResponse);
-						}
-
-
-					}
-
 			// public
 
 				this.getServer = function () {
-					return m_clServer;
+					return http;
 				};
 				
 				this.start = function (p_nPort) {
@@ -531,22 +322,131 @@ q
 								_extractDataPlugins()
 									.then(function () {
 
-										m_clServer = require('http').createServer();
-										
-										m_clServer.listen(p_nPort, function () {
+										app
+
+											.get('/', function (req, res) {
+
+												_readFile('', 'index.html')
+													.then(function (index) {
+
+														_extractPluginsTemplates()
+															.then(function(sHTML) {
+																_sendHTMLResponse(res, 200, index.replace('{{pages}}', sHTML));
+															})
+															.catch(function (error) {
+																_500(res, index.replace('{{pages}}', error));
+															});
+
+													})
+													.catch(function (error) {
+														_500(res, error);
+													});
+													
+											})
+
+											// js
+
+												.get('/js/plugins.js', function (req, res) {
+
+													_extractPluginsJavascripts()
+														.then(function(sJavascript) {
+															_sendJSResponse(res, 200, sJavascript);
+														})
+														.catch(function (error) {
+															_500(res, error);
+														});
+
+												})
+												.get('/js/children.js', function (req, res) {
+
+													_readAllFiles(path.join(m_sDirWeb, 'js'))
+														.then(function (data) {
+															_sendJSResponse(res, 200, data);
+														})
+														.catch(function (error) {
+															_500(res, error);
+														});
+
+												})
+
+											// pictures
+
+												.get('/pictures/favicon.png', function (req, res) {
+													res.sendFile(path.join(m_sDirWeb, 'pictures', 'favicon.png'));
+												})
+
+											// libs
+
+												// css
+
+												.get('/libs/bootstrap.css', function (req, res) {
+
+													_readAllFiles(path.join(m_sDirWeb, 'libs', 'bootstrap', 'css'))
+														.then(function (data) {
+															_sendCSSResponse(res, 200, data);
+														})
+														.catch(function (error) {
+															_500(res, error);
+														});
+
+												})
+
+												// js
+													
+												.get('/libs/jquery.js', function (req, res) {
+
+													_readAllFiles(path.join(m_sDirWeb, 'libs', 'jquery'))
+														.then(function (data) {
+															_sendJSResponse(res, 200, data);
+														})
+														.catch(function (error) {
+															_500(res, error);
+														});
+
+												})
+												.get('/libs/bootstrap.js', function (req, res) {
+
+													_readAllFiles(path.join(m_sDirWeb, 'libs', 'bootstrap', 'js'))
+														.then(function (data) {
+															_sendJSResponse(res, 200, data);
+														})
+														.catch(function (error) {
+															_500(res, error);
+														});
+
+												})
+												.get('/libs/socketio.js', function (req, res) {
+													res.sendFile(path.join(m_sDirWeb, 'libs', 'socketio', 'socket.io.js'));
+												})
+												.get('/libs/angular.js', function (req, res) {
+													res.sendFile(path.join(m_sDirWeb, 'libs', 'angularjs', 'angular.min.js'));
+												})
+												.get('/libs/angular-modules.js', function (req, res) {
+
+													_readAllFiles(path.join(m_sDirWeb, 'libs', 'angularjs', 'modules'))
+														.then(function (data) {
+															_sendJSResponse(res, 200, data);
+														})
+														.catch(function (error) {
+															_500(res, error);
+														});
+
+												})
+
+											// 404
+
+												.use(function (req, res) {
+													_404(req, res);
+												});
+
+										http.listen(p_nPort, function () {
 											m_clLog.success('-- [HTTP server] started');
 										});
-									
-									// requete
 
-										m_clServer.on('request', function (p_clRequest, p_clResponse) {
-											_router(p_clResponse, url.parse(p_clRequest.url).pathname);
-										});
-								
 									deferred.resolve();
 
-									})
-									.catch(deferred.reject);
+								})
+								.catch(deferred.reject);
 									
 						}
 						catch (e) {
