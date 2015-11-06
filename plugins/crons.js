b0005b143738d28b016b0574f5a450aeacb82827
Sébastien VIDAL <svidal@wynd.eu>
2015-11-06 15:13:19 +0100

    intégration des crons

---
+++ plugins/crons.js
@@ -0,0 +1,26 @@
+
+// dépendances
+	
+	var
+		path = require('path'),
+		cronjob = require('cron').CronJob, // https://github.com/ncb000gt/node-cron/blob/master/README.md
+		Logs = require(path.join(__dirname, '..', 'class', 'Logs.js'));
+
+// module
+	
+	module.exports = function (p_clHTTPSocket, p_clChildSocket, p_clSikyAPI) {
+
+		// attributes
+			
+			var
+				m_clLog = new Logs(path.join(__dirname, '..', 'logs', 'plugins', 'crons'));
+				
+		// constructor
+
+			/*var job = new cronjob('* 00 * * * * *', function() {
+			  	console.log(new Date());
+			}, function () {
+		    	console.log('done');
+			}, true);*/
+
+	};
