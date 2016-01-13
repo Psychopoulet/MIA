25c0f97fbd71f56a60cd04161cc20f4dbbeac4b2
Sébastien VIDAL <svidal@wynd.eu>
2016-01-13 18:27:50 +0100

    ajout d'une alert perso

--- plugins/crons/crons.js
+++ plugins/crons/crons.js
@@ -17,10 +17,26 @@
 				
 		// constructor
 
-			var job = new cronjob('00 00 16 * * 1-5', function() {
+			new cronjob('00 00 16 * * 1-5', function() {
 
-				Container.get('server.socket.child').emit('media.video.play', {"name":"test","url":"https://www.youtube.com/watch?v=zIA0kaGFIhQ","urlembeded":"https://www.youtube.com/embed/zIA0kaGFIhQ","code":"hLcJuWVd6hw"});
-			  	
+				Container.get('server.socket.child').emit('media.video.play', {
+					"name":"Café !",
+					"url":"https://www.youtube.com/watch?v=zIA0kaGFIhQ",
+					"urlembeded":"https://www.youtube.com/embed/zIA0kaGFIhQ",
+					"code":"hLcJuWVd6hw"
+				});
+
+			}, null, true);
+
+			new cronjob('00 00 12 * * 1-5', function() {
+
+				Container.get('server.socket.child').emit('media.video.play', {
+					"url":"https://www.youtube.com/watch?v=2KqsR_ko5TU",
+					"name":"Manger !",
+					"urlembeded":"https://www.youtube.com/embed/2KqsR_ko5TU",
+					"code":"2KqsR_ko5TU"
+				});
+
 			}, null, true);
 
 	};
