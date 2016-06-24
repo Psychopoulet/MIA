5ae7fe557b59933f29e6de12e1b582c2fb1793d5
Sébastien Vidal <Sbastien Vidal>
2016-06-24 18:55:21 +0200

    renommages, début de réécriture pour intégration de node-scenarios

--- database/create.sql
+++ database/create.sql
@@ -45,23 +45,3 @@ CREATE TABLE IF NOT EXISTS crons (
 	timer VARCHAR(50) NOT NULL,
 	FOREIGN KEY(id_user) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-
--- basic data
-
-INSERT INTO users (login, password, email) VALUES
-('rasp', 'd74bc8c7cb18433b140785ae51c48c77b4dc2208', '');
-
-INSERT INTO actionstypes (name, command) VALUES
-('Jouer un son', 'media.sound.play'),
-('Jouer une vidéo', 'media.video.play'),
-('Lire un texte', 'tts');
-
-INSERT INTO crons (id_user, name, timer) VALUES
-(1, 'Café !!', '00 00 16 * * 1-5'),
-(1, 'Manger !!', '00 30 12 * * 1-5');
-
-INSERT INTO status (code, name, backgroundcolor, textcolor) VALUES
-('ACCEPTED', 'Accepté(e)', '#dff0d8', '#3c763d'),
-('BLOCKED', 'Bloqué(e)', 'red', 'black'),
-('WAITING', 'En attente', '#fcf8e3', '#8a6d3b');
